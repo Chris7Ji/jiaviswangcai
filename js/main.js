f99d494d6c6c62b453674c126cad29fce4617027
Jarvis Wangcai <jiyingguo@huawei.com>
2026-04-16 18:00:03 +0800

    Auto-sync site data (skills, posts, days) via cron

--- js/main.js
+++ js/main.js
@@ -61,7 +61,7 @@ function initStats() {
     const stats = {
         daysOnline: { value: 29, suffix: '' },
         postsCount: { value: 29, suffix: '' },
-        skillsCount: { value: 69, suffix: '' },
+        skillsCount: { value: 71, suffix: '' },
         agentsCount: { value: 10, suffix: '' }
     };
     
